f0e8284a9a379843782e60e3c6703e7d1798b209
GY <GY>
2021-09-03 22:12:14 -0400

    1 [CHG] using EF;

--- Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
+++ Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.DebugConsole
 {
@@ -10,6 +12,29 @@ namespace Vic.SportsStore.DebugConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("start...");
+
+            using (var ctx = new EFDbContext())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var product = new Product
+                    {
+                        Name = $"Football{i}",
+                        Price = i + 12,
+                        Description = $"this is football {i}"
+                    };
+
+                    ctx.Products.Add(product);
+                }
+
+
+
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("done");
+            Console.ReadLine();
         }
     }
 }
