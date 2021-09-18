4f26da535cce04878e62d08d758d92def1728003
GY <GY>
2021-09-17 20:30:42 -0400

    1 [CHG] add paging links;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
 using Vic.SportsStore.Domain.Entities;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -15,15 +16,30 @@ namespace Vic.SportsStore.WebApp.Controllers
 
         public IProductsRepository Repository { get; set; }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             var result = Repository
                 .Products
+                .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductId)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize);
 
-            return View(result);
+            var pagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = Repository.Products.Count(),
+            };
+
+            var vm = new ProductsListViewModel
+            {
+                Products = result,
+                PagingInfo = pagingInfo,
+                CurrentCategory = category,
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Models/ProductsListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.WebApp.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
